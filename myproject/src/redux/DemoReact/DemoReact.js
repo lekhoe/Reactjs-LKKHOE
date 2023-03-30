a2f3a1fa3d0f4e2903b2d1397384c483a3aac64d
lekhoe <lekhoeth@gmail.com>
2023-03-30 22:03:36 +0700

    Khỏe đẩy code

---
+++ myproject/src/redux/DemoReact/DemoReact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DemoReact() {
+  return (
+    <>
+      <button>-</button>
+      <input />
+      <button>+</button>
+    </>
+  );
+}
+
+export default DemoReact;
